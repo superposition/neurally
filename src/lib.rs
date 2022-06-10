878c3bc50bb8488dab5f52573adc72cb9b986c17
Eric Manganaro <ericmanganaro@gmail.com>
2022-06-09 20:14:43 -0400

    mnist dataframe head

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+use polars::prelude::*;
+use ndarray::prelude::*;
+use std::fs::File;
+
+
+fn mnist_df() -> Result<DataFrame> {
+    let file = File::open("mnist.csv")
+                    .expect("could not read file");
+
+    CsvReader::new(file)
+            .infer_schema(None)
+            .has_header(true)
+            .finish()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+
+        let df = mnist_df().unwrap();
+        println!("{:?}", df.head(Some(10)));        
     }
 }
